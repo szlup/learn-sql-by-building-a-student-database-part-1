4521e2772c9f603040dc263e4eff223eb72a7695
Xac Williams <whitney.zac@gmail.com>
2025-04-06 00:59:58 +0000

    Save progress: 1033.1

--- insert_data.sh
+++ insert_data.sh
@@ -63,7 +63,8 @@ do
   if [[ $FIRST != first_name ]]
   then
     #get major_id
-    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")"
+    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    echo $MAJOR_ID
 
     #if not found
     #set to null
